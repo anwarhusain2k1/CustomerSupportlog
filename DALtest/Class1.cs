d7788448c607181affc800191b2d3ddeb8972c50
anwarhusain2k1 <anwar2k1@gmail.com>
2022-11-03 10:34:08 +0530

    testcase

--- DALtest/Class1.cs
+++ DALtest/Class1.cs
@@ -18,12 +18,13 @@ namespace DALtest
         }
         
         [TestCase(101,"password")]
+        [TestCase(102, "login")]
         public void check(int id,string password)
         {
             UserInfo u = new UserInfo();
             u.UserId = id;
             u.Password = password;
-            Assert.IsTrue(d.login(u));
+            Assert.IsTrue(d.login(id,password));
         }
     }
 }
